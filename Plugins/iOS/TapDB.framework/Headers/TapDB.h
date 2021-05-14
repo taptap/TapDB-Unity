da54f588ab600270d93195040000836013dc5fdd
zhangwenyan <zhangwenyan@xd.com>
2021-05-14 15:41:58 +0800

    feat(*):update framework

--- Plugins/iOS/TapDB.framework/Headers/TapDB.h
+++ Plugins/iOS/TapDB.framework/Headers/TapDB.h
@@ -22,7 +22,7 @@ FOUNDATION_EXPORT TapDBLoginType const TapDBLoginTypeTwitter;
 FOUNDATION_EXPORT TapDBLoginType const TapDBLoginTypePhoneNumber;
 
 #define TapDBSDK               @"TapDB"
-#define TapDBSDK_VERSION_NUMBER @"20101003"
+#define TapDBSDK_VERSION_NUMBER @"20101006"
 #define TapDBSDK_VERSION        @"2.1.1"
 //版本号
 static NSString *const version = @"3.0.3";
