8b7d55431d62edf1b49d867bcd3974c37e4852c2
xiaoyi <xiaoyi@xd.com>
2022-01-07 17:21:46 +0800

    feat:update upm

---
+++ Editor/TapDBIOSProcessor.cs
@@ -0,0 +1,38 @@
+using System.IO;
+using TapTap.Common.Editor;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+namespace TapTap.TapDB.Editor
+{
+#if UNITY_IOS
+    public class TapDBIOSProcessor
+    {
+        [PostProcessBuild(104)]
+        public static void OnPostprocessBuild(BuildTarget buildTarget, string path)
+        {
+            if (buildTarget != BuildTarget.iOS) return;
+
+            var projPath = TapCommonCompile.GetProjPath(path);
+            var proj = TapCommonCompile.ParseProjPath(projPath);
+            var target = TapCommonCompile.GetUnityTarget(proj);
+            var unityFrameworkTarget = TapCommonCompile.GetUnityFrameworkTarget(proj);
+            if (TapCommonCompile.CheckTarget(target))
+            {
+                Debug.LogError("Unity-iPhone is NUll");
+                return;
+            }
+            
+            proj.AddFrameworkToProject(unityFrameworkTarget, "AdSupport.framework", false);
+            proj.AddFrameworkToProject(unityFrameworkTarget, "CoreMotion.framework", false);
+            proj.AddFrameworkToProject(unityFrameworkTarget, "Security.framework", false);
+            proj.AddFrameworkToProject(unityFrameworkTarget, "SystemConfiguration.framework", false);
+            proj.AddFrameworkToProject(unityFrameworkTarget, "AppTrackingTransparency.framework", true);
+            proj.AddFrameworkToProject(unityFrameworkTarget, "iAd.framework", false);
+            proj.AddFrameworkToProject(unityFrameworkTarget, "AdServices.framework", true);
+            File.WriteAllText(projPath, proj.WriteToString());
+        }
+    }
+#endif
+}
